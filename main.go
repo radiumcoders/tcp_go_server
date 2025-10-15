288d28bf550efbe0ccb5fd137970bc0a149931e4
radiumcoders <radiumcoders@gmail.com>
2025-10-15 15:33:04 +0530

    server and connection setup :D

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
+// what to achive ?
+// task : 1 get the args from the command line if less than 2 exit and print usage
+// task : 2 get the port from the command line and start a server on that port
 func main() {
-	fmt.Println("tcp server start")
+	if len(os.Args) < 2 {
+		fmt.Println("Use: go run main.go <port> pls :D")
+		os.Exit(1)
+	}
+	port := fmt.Sprintf(":%s", os.Args[1])
+	listener , err := net.Listen("tcp" , port)
+	if err != nil {
+		fmt.Println("error occured D: err --> ", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("Server started on port", port)
+	// handle connections
+	for {
+		conn , err := listener.Accept()
+		if err != nil {
+			fmt.Println("error occured D: err --> ", err)
+			continue
+		}
+		fmt.Println(conn)
+	}
 }
